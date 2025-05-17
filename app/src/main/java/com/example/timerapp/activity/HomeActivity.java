dfec799828c3a22bc96a5ff6fb4909386a8caea5
HYN8055 <hyn09102004@gmail.com>
2025-05-17 10:40:22 +0700

    Thay đổi Utils.java

--- app/src/main/java/com/example/timerapp/activity/HomeActivity.java
+++ app/src/main/java/com/example/timerapp/activity/HomeActivity.java
@@ -2,37 +2,44 @@ package com.example.timerapp.activity;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.timerapp.R;
 import com.example.timerapp.Task;
 import com.example.timerapp.TaskAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class HomeActivity extends BaseActivity {
+public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private TaskAdapter taskAdapter;
+    private List<Task> taskList;
+
     @Override
-    protected int getLayoutResourceId() {
-        return R.layout.activity_homepage;
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_homepage, container, false);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        recyclerView = view.findViewById(R.id.recyclerViewTasks);
 
-        recyclerView = findViewById(R.id.recyclerViewTasks);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         taskList = new ArrayList<>();
-        taskList.add(new Task("Học bài", "00:01:00",false));
-        taskList.add(new Task("Đọc sách", "01:00:00",false));
-        taskList.add(new Task("Thiền", "02:00:00",false));
-        taskList.add(new Task("Tập gym", "02:00:00",false));
-        taskList.add(new Task("Yoga", "02:00:00",false));
-
-        taskAdapter = new TaskAdapter(this, taskList);
+        taskAdapter = new TaskAdapter(getContext(), taskList);
         recyclerView.setAdapter(taskAdapter);
+
+        taskList.add(new Task("Học bài", "00:01:00", false));
+        taskList.add(new Task("Đọc sách", "01:00:00", false));
+        taskAdapter.notifyDataSetChanged();
+
+        return view;
     }
 }
+
--- app/src/main/java/com/example/timerapp/utils/Utils.java
+++ app/src/main/java/com/example/timerapp/utils/Utils.java
@@ -5,6 +5,6 @@ import com.example.timerapp.model.User;
 import java.util.List;
 
 public class Utils {
-    public static final String BASE_URL="http://172.30.152.150/timeapp/";
+    public static final String BASE_URL="http://10.0.0.2/timeapp/";
     public static User user_current=new User();
 }
